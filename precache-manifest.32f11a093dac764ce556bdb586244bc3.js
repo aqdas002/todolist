030b0306967ec770cda80f4fd682f4c5c31c42a2
aqdas002 <42019890+aqdas002@users.noreply.github.com>
2020-06-07 02:03:34 +0530

    Updates

---
+++ precache-manifest.32f11a093dac764ce556bdb586244bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddab082266d6b7cddc7814a5b8c1bfe",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e0b88939b6b9b701a4f1",
+    "url": "/todolist/static/css/main.0da24837.chunk.css"
+  },
+  {
+    "revision": "4a613a8418dd40223cbf",
+    "url": "/todolist/static/js/2.7928337b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.7928337b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b88939b6b9b701a4f1",
+    "url": "/todolist/static/js/main.8fd63ad6.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  },
+  {
+    "revision": "8f88dc492dbc4fed13c9763f1b4ceb39",
+    "url": "/todolist/static/media/pic01.8f88dc49.jpg"
+  }
+]);
